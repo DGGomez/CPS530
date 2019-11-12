80133ee572e989fd9745ffb119d0e5277c9e1178
Daniel Gomez <dggomez21@gmail.com>
2019-11-12 17:30:44 -0500

    intial setup

--- main/src/App.js
+++ main/src/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import {connect} from "react-redux";
 
 import DashboardPage from "./pages/MainPage";
-import ReportPage from "./pages/ReportPage";
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-    <Router>
-      <div style={{ marginTop: '7rem' }}>
-          <NavBar />
-          <Route path="/" exact component={DashboardPage} />
-          <Route path="/dashboard" exact component={DashboardPage} />
-          <Route path="/report" exact component={ReportPage} />
-          <Footer/>
-      </div>
-    </Router> 
-  </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+        <div>
+          <Router>
+            <NavBar/>
+
+                <Route path="/" exact component={DashboardPage} />
+                <Route path="/dashboard" exact component={DashboardPage} />
+                <Footer/>
+
+          </Router> 
+        </div>
+    );
+  }
 }
 
 export default App;
--- main/src/index.js
+++ main/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+import STORE from './store';
 
-ReactDOM.render(<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store={STORE}><App /></Provider>,
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
