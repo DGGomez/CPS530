e4de472b5e6972836372544074f373be86e1ca87
Daniel Gomez <dggomez21@gmail.com>
2019-11-19 13:31:22 -0500

    added tutorial pages instructions

---
+++ tutorial/src/pages/Page3.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+
+class Page3 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        name: '',
+        message: '',
+        email: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange = e =>{
+      this.setState({[e.target.name]: e.target.value})
+  }
+
+  async handleSubmit(e){
+      e.preventDefault()
+      const { name, email, message } = this.state
+  }
+  render() {
+    return (
+        <div>
+            <p>Page1</p>
+        </div>
+    );
+  }
+}
+
+export default connect(null)(Page3);
